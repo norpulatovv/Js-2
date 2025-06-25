6fad765168b316c03f2aff287c5d5bd19607f517
norpulatovv <yoldoshevxasan98@gmail.com>
2025-06-25 16:00:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let ism = prompt("String typedagi malumot kiriting")
+
+let n = prompt("Number typedagi malumot kiriting")
+
+let b = prompt("boolean typedagi malumot kiriting")
+
+confirm("hamma malumotlarni to'g'ri kiritganizga ishonchingiz komilmi?")
+
+alert(`siz kiritgan ${ism} sozi string typega tegishli /n
+    siz kiritgan ${n} sozi number typega tegishli /n
+    siz kiritgan ${b} sozi boolean typega tegishli
+    `)
